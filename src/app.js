0d163f06aea8dbb5e839debf3782588aad0a10c6
Alejandrobv25 <alexbavierasaiz@gmail.com>
2024-11-26 10:22:46 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,10 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let who = ["Jack Sparrow", "Barbossa", "Will Turner", "Calypso"];
-  let action = ["me robó", "secuestró", "encontró"];
-  let what = ["la Perla Negra", "el mapa", "el tesoro"];
-
+  let who = [
+    "Harry Potter",
+    "Darth Vader",
+    "Simba",
+    "Rocky Balboa",
+    "Indiana Jones"
+  ];
+  let action = [
+    "corre por la jungla",
+    "explora una galaxia",
+    "lucha contra el mal",
+    "salta entre edificios",
+    "se enfrenta a un monstruo"
+  ];
+  let what = [
+    "con un hechizo",
+    "con una espada laser",
+    "en una invasion alienígena",
+    "en una batalla épica",
+    "con una brújula mágica"
+  ];
   let randomwho = Math.floor(Math.random() * who.length);
   let randomaction = Math.floor(Math.random() * action.length);
   let randomwhat = Math.floor(Math.random() * what.length);
