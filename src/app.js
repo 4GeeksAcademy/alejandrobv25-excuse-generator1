6166a6701e469fc29069a6582daf5279b26c5138
Alejandrobv25 <alexbavierasaiz@gmail.com>
2024-12-07 12:28:23 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -34,4 +34,15 @@ window.onload = function() {
   document.querySelector(
     "#excuse"
   ).innerHTML = `${who[randomwho]} ${action[randomaction]} ${what[randomwhat]}!`;
+
+  function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  const card = document.querySelector(".card");
+  card.style.backgroundColor = getRandomColor();
 };
